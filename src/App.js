3b18a2dd0b4ae857b51202d0c30fb5ad19389eb5
RANJITHKUMAR-P <ranjithkumar1772002@gmail.com>
2025-09-04 11:18:06 +0530

    addconfig

--- src/App.js
+++ src/App.js
@@ -1,47 +1,3 @@
-// // import Home from "./pages/Home";
-
-// // function App() {
-// //   return <Home />;
-// // }
-
-// // export default App;
-
-
-// import React, { useState } from 'react';
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import Home from './pages/Home';
-// import Projects from './components/Projects';
-// import ProjectDetail from './pages/ProjectDetail';
-// import './App.css';
-
-// function App() {
-//   const [currentPage, setCurrentPage] = useState('home');
-
-//   const renderPage = () => {
-//     if (currentPage === 'home') {
-//       return <Home />;
-//     } else if (currentPage === 'projects') {
-//       return <Projects />;
-//     }
-//   };
-
-//   return (
-//     <Router>
-//       <div className="App">
-     
-        
-//         <Routes>
-//           <Route path="/" element={<Home />} />
-//           <Route path="/projects" element={<Projects />} />
-//           <Route path="/project/:id" element={<ProjectDetail />} />
-//         </Routes>
-//       </div>
-//     </Router>
-//   );
-// }
-
-// export default App;
-
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
@@ -53,7 +9,7 @@ import Freelance from './pages/Freelance';
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <div className="App bg-black min-h-screen">
         <Routes>
           <Route path="/" element={<Home />} />
